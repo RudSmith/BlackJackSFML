f17783492d38c00b90f22807dbb742daf9c5e55a
RudSmith <igor200rudenskiy@gmail.com>
2020-11-04 13:01:19 +0300

    Added method, that returns card points

--- Card.cpp
+++ Card.cpp
@@ -25,6 +25,46 @@ const Card::value_t &Card::value() const
 	return this->m_value;
 }
 
+const Card::points_t& Card::points() const
+{
+	switch (this->m_value)
+	{
+	case 0:
+		return 2;
+		break;
+	case 1:
+		return 3;
+		break;
+	case 2:
+		return 4;
+		break;
+	case 3:
+		return 5;
+		break;
+	case 4:
+		return 6;
+		break;
+	case 5:
+		return 7;
+		break;
+	case 6:
+		return 8;
+		break;
+	case 7:
+		return 9;
+		break;
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+		return 10;
+		break;
+	case 12:
+		return 11;
+
+	}
+}
+
 
 void Card::loadTexture(const std::string& fileName)
 {
--- Card.h
+++ Card.h
@@ -6,12 +6,14 @@ class Card
 {
 public:
 	using value_t = unsigned short;
+	using points_t = unsigned short;
 
 	Card(value_t value, const std::string &textureFileName);
 
 	const sf::Sprite &sprite() const;
 	const sf::Texture &texture() const;
 	const value_t &value() const;
+	const points_t &points() const;
 
 	void loadTexture(const std::string &fileName);
 	void setSpriteFromTexture();
