72fbaf25f5477fae3fc89ed0a75c0203e984e687
RudSmith <igor200rudenskiy@gmail.com>
2020-12-09 13:01:43 +0300

    Refactored button class

--- Button.cpp
+++ Button.cpp
@@ -3,67 +3,71 @@
 Button::Button(float x, float y, float width, float height, sf::Font* font, std::string text,
 				sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor)
 {
-	this->m_shape.setPosition(sf::Vector2f(x, y));
-	this->m_shape.setSize(sf::Vector2f(width, height));
+	// Set the position and the size 
+	m_shape.setPosition(sf::Vector2f(x, y));
+	m_shape.setSize(sf::Vector2f(width, height));
 
-	this->m_font = font;
-	this->m_text.setFont(*this->m_font);
-	this->m_text.setString(text);
-	this->m_text.setFillColor(sf::Color::Black);
-	this->m_text.setCharacterSize(20);
-	this->m_text.setPosition(
-		this->m_shape.getPosition().x + (this->m_shape.getGlobalBounds().width / 2.f) - this->m_text.getGlobalBounds().width / 2.f,
-		this->m_shape.getPosition().y + (this->m_shape.getGlobalBounds().height / 2.f) - this->m_text.getGlobalBounds().height / 2.f
-	);
+	// Initialize the font
+	m_font = font;
+	m_text.setFont(*m_font);
+	m_text.setString(text);
+	m_text.setFillColor(sf::Color::Black);
+	m_text.setCharacterSize(20);
+	m_text.setPosition(
+		m_shape.getPosition().x + (m_shape.getGlobalBounds().width / 2.f) - m_text.getGlobalBounds().width / 2.f,
+		m_shape.getPosition().y + (m_shape.getGlobalBounds().height / 2.f) - m_text.getGlobalBounds().height / 2.f);
 
-	this->m_idleColor = idleColor;
-	this->m_hoverColor = hoverColor;
-	this->m_activeColor = activeColor;
+	// Set colours
+	m_idleColor = idleColor;
+	m_hoverColor = hoverColor;
+	m_activeColor = activeColor;
+	m_shape.setFillColor(this->m_idleColor);
 
-	this->m_shape.setFillColor(this->m_idleColor);
-
-	this->m_state = ButtonState::BTN_IDLE;
+	// Set default state
+	m_state = ButtonState::BTN_IDLE;
 }
 
+// Update current button state
 void Button::update(const sf::Vector2f mousePos)
 {
-	this->m_state = ButtonState::BTN_IDLE;
+	m_state = ButtonState::BTN_IDLE;
 
-	if (this->m_shape.getGlobalBounds().contains(mousePos))
+	if (m_shape.getGlobalBounds().contains(mousePos))
 	{
-		this->m_state = ButtonState::BTN_HOVER;
+		m_state = ButtonState::BTN_HOVER;
 
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
-			this->m_state = ButtonState::BTN_ACTIVE;
+			m_state = ButtonState::BTN_ACTIVE;
 		}
 	}
 
-	switch (this->m_state)
+	switch (m_state)
 	{
 	case ButtonState::BTN_IDLE:
-		this->m_shape.setFillColor(m_idleColor);
+		m_shape.setFillColor(m_idleColor);
 		break;
 
 	case ButtonState::BTN_HOVER:
-		this->m_shape.setFillColor(m_hoverColor);
+		m_shape.setFillColor(m_hoverColor);
 		break;
 
 	case ButtonState::BTN_ACTIVE:
-		this->m_shape.setFillColor(m_activeColor);
+		m_shape.setFillColor(m_activeColor);
 		break;
 	}
 }
 
+// Render button to the target
 void Button::render(sf::RenderTarget* target)
 {
-	target->draw(this->m_shape);
-	target->draw(this->m_text);
+	target->draw(m_shape);
+	target->draw(m_text);
 }
 
 const bool Button::isPressed() const
 {
-	if (this->m_state == ButtonState::BTN_ACTIVE)
+	if (m_state == ButtonState::BTN_ACTIVE)
 		return true;
 
 	return false;
--- Button.h
+++ Button.h
@@ -5,6 +5,7 @@
 
 class Button
 {
+	// Current button state
 	enum class ButtonState
 	{
 		BTN_IDLE = 0,
@@ -13,25 +14,33 @@ class Button
 	};
 
 public:
+	// Create button with given coords and size
 	Button(float x, float y, float width, float height,
 		sf::Font *font, std::string text = 0, sf::Color idleColor = sf::Color::White,
 		sf::Color hoverColor = sf::Color::White, sf::Color activeColor = sf::Color::White);
 
+	// Update button state
 	void update(const sf::Vector2f mousePos);
+	// Display the button
 	void render(sf::RenderTarget* target);
 
+	// Check if button is currently pressed
 	const bool isPressed() const;
 
 
 private:
+	// The shape of the button
 	sf::RectangleShape m_shape;
+	// Font on the button
 	sf::Font *m_font;
+	// Text of the button
 	sf::Text m_text;
-
+	
 	sf::Color m_idleColor;
 	sf::Color m_hoverColor;
 	sf::Color m_activeColor;
 
+	// Current state of the button
 	ButtonState m_state;
 };
 
