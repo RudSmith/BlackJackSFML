064b07f53f625de1e6a296d989ac40b9cf8ae616
RudSmith <igor200rudenskiy@gmail.com>
2020-12-09 13:13:49 +0300

    Added comments to deck class

--- Deck.cpp
+++ Deck.cpp
@@ -4,6 +4,7 @@
 
 Deck::Deck()
 {
+	// Inititalize the deck
 	this->init();
 	this->m_top_card_index = 0;
 	this->shuffle_card_indexes();
@@ -11,22 +12,28 @@ Deck::Deck()
 
 Deck::~Deck()
 {
+	// Destroy all the cards;
 	for (Card* &iter_card : m_deck)
 		delete iter_card;
 }
 
 void Deck::init()
 {
+	// Load the textures from a file
 	const std::string file_path{ "card_textures.txt" };
 	std::fstream card_textures_file;
 	card_textures_file.open(file_path, std::ios::in);
 
+	// Current card index in the loop
 	size_t card_index{};
+	// Max card dignity
 	const size_t max_dignity{ 13 };
+	// Max card suit
 	const size_t max_suit{ 4 };
 
 	const size_t streamsize_count{ 16 };
 
+	// Loop goes for 52 times and inits each card
 	for (size_t suit{}; suit < max_suit; ++ suit)
 		for (size_t dignity{}; dignity < max_dignity; ++dignity)
 		{
@@ -42,6 +49,7 @@ void Deck::init()
 
 void Deck::shuffle_card_indexes()
 {
+	// Init indexes array
 	for (size_t iter{}; iter < m_card_indexes.size(); ++iter)
 	{
 		m_card_indexes.at(iter) = iter;
@@ -49,6 +57,7 @@ void Deck::shuffle_card_indexes()
 
 	srand(time(NULL));
 
+	// Shuffle the indexes
 	for (size_t iter{}; iter < m_card_indexes.size(); ++iter)
 	{
 		int swap_index{ rand() % 52 };
@@ -60,16 +69,18 @@ void Deck::shuffle_card_indexes()
 
 const size_t& Deck::pop_top_card()
 { 
-	++this->m_top_card_index;
-	return this->m_card_indexes.at(m_top_card_index - 1);
+	// Remove top card from the deck
+	++m_top_card_index;
+	return m_card_indexes.at(m_top_card_index - 1);
 }
 
 const size_t& Deck::look_at_top_card()
 {
-	return this->m_card_indexes.at(m_top_card_index);
+	// Get info about top card
+	return m_card_indexes.at(m_top_card_index);
 }
 
 Card& Deck::at(const size_t& index)
 {
-	return *this->m_deck.at(index);
+	return *m_deck.at(index);
 }
--- Deck.h
+++ Deck.h
@@ -3,24 +3,39 @@
 #include <array>
 #include "Card.h"
 
+/*
+	NOTE: We have 2 arrays, because we cannot swap card textures properly
+	So, we have not to shuffle cards, but shuffle their positions in different array
+*/
+
 class Deck
 {
 public:
+	// Create deck
 	Deck();
+	// Destroy deck
 	~Deck();
 
+	// Init the deck
 	void init();
+	// Shuffle the deck
 	void shuffle_card_indexes();
 
+	// Remove top card from the deck
 	const size_t &pop_top_card();
+	// Get info about top card of the deck
 	const size_t &look_at_top_card();
 
+	// Get the reference to the card in deck by it`s index
 	Card& at(const size_t& index);
 
 private:
+	// Array of cards
 	std::array<Card*, 52> m_deck;
+	// Array of card indexes in deck
 	std::array<size_t, 52> m_card_indexes;
 
+	// Current top card
 	size_t m_top_card_index;
 };
 
