a7cd3943602adcf36ed516046bf545bc2a5a666d
RudSmith <igor200rudenskiy@gmail.com>
2020-12-13 22:26:37 +0300

    Remove console window in debug profile

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include "MainMenu.h"
 
+#ifdef _DEBUG
+#pragma comment( linker, "/subsystem:console" )
 int main()
+
+#else
+
+#include <Windows.h>
+
+#pragma comment( linker, "/subsystem:windows" )
+
+int  WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nShowCmd
+)
+#endif
 {
 	MainMenu gamemenu;
 	gamemenu.run();
